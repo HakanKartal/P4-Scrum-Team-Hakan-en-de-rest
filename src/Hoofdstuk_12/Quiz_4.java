bbdd74466696cede1a8d5bbb99b8f919b3d73982
wouter0303 <wouteroelemans@gmail.com>
2019-05-15 10:38:46 +0200

    h12 quiz 4

--- src/Hoofdstuk_12/Quiz_4.java
+++ src/Hoofdstuk_12/Quiz_4.java
@@ -1,5 +1,10 @@
 package Hoofdstuk_12;
 
-public class Quiz_4 {
-
+public class Quiz_4 
+{
+	final void methodeA(int x, double y)
+	{
+		double z = x * y;
+		System.out.print(z);
+	}
 }
---
+++ src/Hoofdstuk_12/Quiz_4_1.java
@@ -0,0 +1,15 @@
+package Hoofdstuk_12;
+
+public class Quiz_4_1 extends Quiz_4 
+{
+	void methodeA(int x, int y)
+	{
+		int z = x - y;
+		System.out.print(z);
+	}
+	public static void main(String[] args)
+	{
+		Quiz_4_1 mk = new Quiz_4_1();
+		mk.methodeA(5,3.0);
+	}
+}
