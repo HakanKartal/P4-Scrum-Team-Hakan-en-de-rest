c3a744a3b24bfaca0ea9e0cc49fd1a3549e5919e
wouter0303 <wouteroelemans@gmail.com>
2019-05-16 13:03:18 +0200

    quiz 8 af

--- src/Hoofdstuk_11/Quiz_8.java
+++ src/Hoofdstuk_11/Quiz_8.java
@@ -4,8 +4,10 @@ public class Quiz_8
 {
 	public String str1 = "1";
 	protected String str2 = "2";
+	protected String mijnInt = "6";
 	String str3 = "3";
 	private String str4 = "4";
 }
 
 // de uitkomst = 1 2
+//als ik een protected string toevoeg kan ik zowel supK als subK gebreuken
