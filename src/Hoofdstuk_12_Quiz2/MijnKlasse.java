f5565bdfe6f2edba6bf3f9caa28270224b8ddce5
Noelleyasmijn <47447095+Noelleyasmijn@users.noreply.github.com>
2019-05-16 13:00:31 +0200

    Quiz 2 af

--- src/Hoofdstuk_12_Quiz2/MijnKlasse.java
+++ src/Hoofdstuk_12_Quiz2/MijnKlasse.java
@@ -3,7 +3,15 @@ package Hoofdstuk_12_Quiz2;
 public class MijnKlasse extends Superklasse
 {
   int z = 5;
-  void mijnMethode(int x)
+  int methodeB(int x)
+  {
+	  System.out.print("x");
+	  return x;
+  }
+  
+  //nee, we kunnen deze methode niet toevoegen aan MijnKlasse, omdat hij de Superklasse probeert te overriden, maar dat kan niet.
+  
+  void mijnKlasse(int x)
   {
     System.out.print("x");
   }
